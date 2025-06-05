30bab78b98b5e0a9047af5cd94e1e40e419ac9a9
Scorpi <scorpifr@gmail.com>
2025-06-05 16:10:35 +0200

    style error on menu bars. corrected.

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -2,7 +2,7 @@ import Item from "./Item.js";
 
 export default function ItemList({ items, imageDirectory }) {
   return (
-    <div>
+    <div className="flex-container2">
       {items.map(function (item, index) {
         return (
           <Item item={item} imageDirectory={imageDirectory} key={item.id} />
