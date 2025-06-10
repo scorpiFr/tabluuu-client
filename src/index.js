e5a78bf2f167ef0531e28dc140e1c187dd093cda
Scorpi <scorpifr@gmail.com>
2025-06-10 14:24:21 +0200

    add router .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Myrouteur from "./Myrouteur";
 //import reportWebVitals from './reportWebVitals';
 // <React.StrictMode>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Myrouteur />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
